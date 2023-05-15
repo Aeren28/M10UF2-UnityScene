af6a193de87bc3939fd67cfd9a98e0397ef2558d
PC-imatge-2022-2023 <licenses@enti.cat>
2023-05-15 20:35:07 +0200

    no message

--- Assets/Scripts/EnvironmentChange.cs
+++ Assets/Scripts/EnvironmentChange.cs
@@ -8,17 +8,24 @@ public class EnvironmentChange : MonoBehaviour
     public AudioMixerSnapshot indoor;
     public AudioMixerSnapshot outdoor;
 
+    public int house;
+
     public float transitionTime = 0.25f;
 
     void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.tag == "Indoor")
+        if (collider.gameObject.tag == "House")
+        {
             indoor.TransitionTo(transitionTime);
+            house++;
+        }
     }
 
     void OnTriggerExit(Collider collider)
     {
-        if (collider.gameObject.tag == "Indoor")
+        if (collider.gameObject.tag == "House" && house <= 0)
             outdoor.TransitionTo(transitionTime);
+        else if (collider.gameObject.tag == "House" && house > 0)
+            house--;
     }
 }
